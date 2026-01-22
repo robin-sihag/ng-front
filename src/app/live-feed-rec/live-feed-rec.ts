0bf0be482db77ec5c53bdfbe0dacdb696e8f648a
Robin Sihag <robinsihagxp18@gmail.com>
2026-01-22 23:24:23 +0530

    live-feed-rec component added

--- src/app/live-feed-rec/live-feed-rec.ts
+++ src/app/live-feed-rec/live-feed-rec.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-live-feed-rec',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './live-feed-rec.html',
   styleUrl: './live-feed-rec.scss',
 })
 export class LiveFeedRec {
+  constructor(private fb: FormBuilder) {}
+  formModel!: FormGroup;
 
+  ngOnInit() {
+    this.formModel = this.fb.group({
+      firstName: [
+        'Default value',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(10)],
+      ],
+      lastName: [
+        '',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(10)],
+      ],
+      waveGreeting: [false],
+    });
+  }
+
+  onSubmit(){}
 }
